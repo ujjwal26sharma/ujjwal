d543f9733645b868f5098e3a20b0e9b47735f7b4
213vaibhav <122852951+213vaibhav@users.noreply.github.com>
2023-03-24 14:40:11 +0530

    Update typingspeed.py

--- typingspeed.py
+++ typingspeed.py
@@ -1,4 +1,18 @@
+from time import time
 
+# calculate the accuracy of input prompt
+def typingErrors(prompt):
+    global iwords
+
+    words = prompt.split()
+    errors = 0
+
+    for i in range(len(iwords)):
+        if i in (0, len(iwords)-1):
+            if iwords[i] == words[i]:
+                continue
+            else:
+                errors +=1
         else:
             if iwords[i] == words[i]:
                 if (iwords[i+1] == words[i+1]) & (iwords[i-1] == words[i-1]):
